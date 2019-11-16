816cbd81ff8d36593a56a4affbce767a17b151b7
t7m3 <toda7.mk3@gmail.com>
2019-11-16 16:40:34 +0900

    2019-11-16 １０回目のコミット　弾が上端に達したら、初期位置に戻るようにした。

---
+++ app/src/main/java/com/example/simpleshootinggame_v352/Bullet.kt
@@ -0,0 +1,30 @@
+package com.example.simpleshootinggame_v352
+
+import android.widget.ImageView
+
+class Bullet(image: ImageView, x:Float, y:Float, height:Int) {
+
+    public val imageView = image
+    private val screenHeight = height
+    public var state = "stop"  // 弾の状態を保存する。
+                               //"move"：弾は動いている。"stop"：弾は動いていない。
+    private var xx = x
+    private var yy = y
+
+    init {
+        imageView.x = x;
+        imageView.y = y;
+    }
+
+    public fun move(y:Int){
+
+        imageView.y = imageView.y - y  // yだけ、上に移動する
+
+        if(imageView.y <= 0){  //画面の上端になったら
+            state = "stop"
+            imageView.x = xx // 位置を初期位置にする
+            imageView.y = yy // 位置を初期位置にする
+        }
+
+    }
+}
